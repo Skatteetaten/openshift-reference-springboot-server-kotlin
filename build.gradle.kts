a0caeec3fe3eba5c7070f752503c2508659c00b1
Anders Mikkelsen <Anders.Mikkelsen@skatteetaten.no>
2020-09-14 16:45:46 +0200

    initial

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("idea")
+    id("java")
+    id("no.skatteetaten.gradle.aurora") version("4.0.0rc1")
+}
+
+aurora {
+    useAuroraDefaults
+    useKotlin {
+        useKtLint
+    }
+    useSpringBoot {
+        useCloudContract
+    }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-jdbc:2.3.3.RELEASE")
+    implementation("com.oracle.database.jdbc:ojdbc8:12.2.0.1")
+
+    testImplementation("com.h2database:h2")
+    testImplementation("org.flywaydb:flyway-core")
+}
